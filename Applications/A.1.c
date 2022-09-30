5f8d56c76378c298409ac79ba284165599f34ae6
yasmeen <yasmeenm322@gmail.com>
2022-10-01 00:58:43 +0200

    app modified

--- Applications/A.1.c
+++ Applications/A.1.c
@@ -1,49 +1,29 @@
 #include <stdio.h>
-int binarysearch(int list[], int item)
+
+void Delete_elements(int list[], int position, int n)
 {
-    int low = 0;
-    int high = 9;
-    while (low <= high)
+    if (position < n)
     {
-        int mid = (low + high) / 2;
-        int guess = list[mid];
-        if (guess == item)
-            return mid;
-        if (guess > item)
-            high = mid - 1;
-        if (guess < item)
-            low = mid + 1;
-    }
-    return -1;
-}
-void Delete_elements(int list[],int item,int n)
-{
-    int position = binarysearch(list, item);
-    if (position == -1)
-    {
-        printf("the item is not present");
-    }
-    else
-    {
-        for (int i = position; i< n; i++)
+        for (int i = position - 1; i < n - 1; i++)
         {
             list[i] = list[i + 1];
         }
 
-        n--;
         for (int i = 0; i < n; i++)
         {
             printf("%d ", list[i]);
         }
     }
+    else
+        printf("the deletion can not happen check if you entered valid position\n");
 }
 int main()
 {
-    int item;
+    int position;
     int list[] = { 10,25,38,49,58,67,75,89,98,107 };
     int n = sizeof(list) / sizeof(list[0]);
-    printf("enter the number you want to delete\n");
-    scanf_s("%d", &item);
-    Delete_elements(list, item, n);
-	return 0;
+    printf("enter the position you want to delete\n");
+    scanf_s("%d", &position);
+    Delete_elements(list, position, n);
+    return 0;
 }
--- Applications/A.2.c
+++ Applications/A.2.c
@@ -1,23 +1,33 @@
 #include <stdio.h>
 int Maximum_unimodal(int arr[], int start, int end)
 {
-    int max = arr[start];
-    for (int i = start + 1; i < end; i++)
+    /* int max = arr[start];
+     for (int i = start + 1; i < end; i++)
+     {
+         if (arr[i] > max)
+             max = arr[i];
+         else
+             break;
+     }
+     return max;*/
+    int mid = (start + end) / 2;
+    if (arr[mid] > arr[mid - 1])
     {
-        if (arr[i] > max)
-            max = arr[i];
+        if (arr[mid] > arr[mid + 1])
+            return arr[mid];
         else
-            break;
+            return Maximum_unimodal(arr, mid + 1, end);
     }
-    return max;
+    else
+        return Maximum_unimodal(arr, start, mid - 1);
 }
 int main()
 {
-    int arr[7];
+    int arr[10];
     int n = sizeof(arr) / sizeof(arr[0]);
     printf("enter the elements of the array ");
     for (int i = 0;i < n;i++)
         scanf_s("%d", &arr[i]);
-    printf("Max element: %d", Maximum_unimodal(arr, 0, n));
+    printf("Max element: %d", Maximum_unimodal(arr, 0, n - 1));
     return 0;
 }
